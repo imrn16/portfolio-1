0f77e4b1cf5608382302d1b40140e5957f8d9203
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-10-05 12:18:44 -0500

    modified:   src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,80 +3,37 @@ import "./globals.css";
 import Head from "next/head";
 
 const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
-export const metadata = {
-	title: "Imran Mohiuddin",
-	description: "Full Stack Developer",
-};
+// export const metadata = {
+//   title: "Imran Mohiuddin",
+//   description: "Full Stack Developer",
+// };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<Head>
-				<meta
-					property="og:title"
-					content="Imran Mohiuddin"
-				/>
-				<meta
-					property="og:description"
-					content="Full Stack Developer"
-				/>
-				<meta
-					property="og:image"
-					content="/twitter-image.jpg"
-				/>
-				<meta
-					property="og:url"
-					content="https://www.imranmohiuddin.com"
-				/>
-				<meta
-					name="twitter:card"
-					content="summary_large_image"
-				/>
-				<meta
-					name="twitter:title"
-					content="Imran Mohiuddin"
-				/>
-				<meta
-					name="twitter:description"
-					content="Full Stack Developer"
-				/>
-				<meta
-					name="twitter:image"
-					content="/twitter-image.jpg"
-				/>
-				<meta
-					property="og:image:alt"
-					content="Imran Mohiuddin"
-				/>
-				<meta
-					property="og:image:width"
-					content="1000"
-				/>
-				<meta
-					property="og:image:height"
-					content="600"
-				/>
-				<meta
-					property="og:image:type"
-					content="image/jpg"
-				/>
-			</Head>
-			<body
-				className={`font-sf-pro bg-slate-900 subpixel-antialiased text-white`}
-				// className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <Head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Imran Mohiuddin" />
+        <meta name="twitter:description" content="Full Stack Developer" />
+        <meta name="twitter:image" content="/twitter-image.jpg" />
+        <meta property="og:title" content="Imran Mohiuddin" />
+        <meta property="og:description" content="Full Stack Developer" />
+        <meta property="og:image" content="/twitter-image.jpg" />
+        <meta property="og:url" content="https://www.imranmohiuddin.com" />
+      </Head>
+      <body className={`font-sf-pro bg-slate-900 subpixel-antialiased text-white`}>
+        {children}
+      </body>
+    </html>
+  );
 }
