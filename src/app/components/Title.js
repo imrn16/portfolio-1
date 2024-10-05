35990661f903064f8b3e30675f434150c2ae2ec4
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-10-05 11:14:25 -0500

    new file:   public/opengraph-image.png 	modified:   src/app/components/Title.js

--- src/app/components/Title.js
+++ src/app/components/Title.js
@@ -4,28 +4,22 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 function Title() {
-	const router = useRouter()
+	const router = useRouter();
 
 	function picClick() {
-		router.push(`/documents/resume.pdf`)
+		router.push(`/documents/resume.pdf`);
 	}
 
 	return (
 		<div className="flex h-auto ">
 			<div className=" w-96 h-48  justify-center items-">
-				{/* <div className="flex flex-row flex-1 h-1/2  items-center justify-left text-7xl tracking-widest font-bold opacity-90">
-					<h1>IMRAN</h1>
-				</div>
-				<div className="flex flex-row h-1/2  items-center justify-left text-5xl tracking-tight pr-2 opacity-90">
-					<h1>MOHIUDDIN</h1>
-				</div> */}
-
 				<button className="flex flex-row  items- justify-left text-4xl tracking-tight font-bold pr-2 opacity-75 md:text-5xl ">
 					<h1> Imran Mohiuddin</h1>
 				</button>
 				<div className="flex flex-row  align-center ">
-					<button className="flex my-2 items- bg-slate-900 hover:opacity-100 opacity-60 rounded-full"
-					onClick={() => picClick()}>
+					<button
+						className="flex my-2 items- bg-slate-900 hover:opacity-100 opacity-60 rounded-full"
+						onClick={() => picClick()}>
 						<Image
 							className="rounded-full "
 							src={"/images/profile.jpg"}
@@ -39,8 +33,21 @@ function Title() {
 
 				<div className="mt-4 opacity-50 text-lg  w-80">{`Building beautiful digital experiences with intuitive user interfaces.`}</div>
 			</div>
+
+
 		</div>
 	);
 }
 
 export default Title;
+
+
+
+{/* <div className="flex flex-col w-96 h-48  justify-center items-center justify-center align-center">
+<button className="flex flex-row  items-center justify-center align-center text-4xl tracking-tight font-bold pr-2 opacity-75 md:text-5xl ">
+	<h1> Imran Mohiuddin</h1>
+</button>
+<div
+	className="flex bg-teal-900 rounded-full p-1 px-4 text-teal-200 mt-2 mr-2 bg-opacity-50 hover:bg-opacity-100 w-48 items-center justify-center align-center"
+	disabled>{`Full Stack Developer`}</div>{" "}
+</div> */}
