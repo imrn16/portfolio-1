1bbad37c7e6536426c86d9ffc58d1844384a0d17
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-10-05 12:08:31 -0500

    modified:   src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Head from "next/head";
 
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -27,18 +26,14 @@ export default function RootLayout({ children }) {
         <meta property="og:description" content="Full Stack Developer" />
         <meta property="og:image" content="/twitter-image.png" />
         <meta property="og:url" content="https://www.imranmohiuddin.com" />
-        <meta name="twitter:card" content="/twitter-image.png" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Imran Mohiuddin" />
         <meta name="twitter:description" content="Full Stack Developer" />
         <meta name="twitter:image" content="/twitter-image.png" />
-        <meta property="twitter:image:alt" content="Imran Mohiuddin" />
         <meta property="og:image:alt" content="Imran Mohiuddin" />
         <meta property="og:image:width" content="1000" />
         <meta property="og:image:height" content="600" />
-        <meta property="og:image:width" content="1000" />
-        <meta property="og:image:height" content="600" />
         <meta property="og:image:type" content="image/png" />
-        <meta property="twitter:image:type" content="image/png" />
       </Head>
       <body
        className={`font-sf-pro bg-slate-900 subpixel-antialiased text-white`}
